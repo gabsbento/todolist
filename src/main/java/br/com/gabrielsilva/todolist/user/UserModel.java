46b258a7ac2943faa35dc706684c3b0f0025b14e
Gabriel Bento <gab.silva.bento@gmail.com>
2024-11-27 22:23:47 -0300

    :sparkles: feat: criada primeira funcionalidade de usuarios

---
+++ src/main/java/br/com/gabrielsilva/todolist/user/UserModel.java
@@ -0,0 +1,11 @@
+package br.com.gabrielsilva.todolist.user;
+
+public class UserModel {
+    private String username;
+    private String name;
+    private String password;
+
+    public String getName(){
+        return this.name;
+    }
+}
