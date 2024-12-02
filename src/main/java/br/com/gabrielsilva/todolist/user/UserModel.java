7f34bf7ecf0698454367a957ab405ac902f29242
Gabriel Bento <gab.silva.bento@gmail.com>
2024-12-02 09:06:42 -0300

    :broom: cleanup: add lombok annotation

--- src/main/java/br/com/gabrielsilva/todolist/user/UserModel.java
+++ src/main/java/br/com/gabrielsilva/todolist/user/UserModel.java
@@ -1,22 +1,11 @@
 package br.com.gabrielsilva.todolist.user;
 
+import lombok.Data;
+
+@Data
 public class UserModel {
     private String username;
     private String name;
     private String password;
 
-    public String getName(){
-        return this.name;
-    }
-    public void setName(String name){
-        this.name = name;
-    }
-    public String getUsername(){return this.username;}
-    public void setUsername(String username) {
-        this.username = username;
-    }
-    public String getPassword(){return this.password;}
-    public void setPassword(String password){
-        this.password = password;
-    }
 }
