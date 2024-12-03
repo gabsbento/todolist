397a4e51191814b96917c7a6159222b1e45843c3
Gabriel Bento <gab.silva.bento@gmail.com>
2024-12-03 09:23:33 -0300

    :sparkles: feat: implementado repository no controller

--- src/main/java/br/com/gabrielsilva/todolist/user/UserController.java
+++ src/main/java/br/com/gabrielsilva/todolist/user/UserController.java
@@ -1,5 +1,6 @@
 package br.com.gabrielsilva.todolist.user;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -8,8 +9,11 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/users")
 public class UserController {
+    @Autowired
+    private IUserRepository iUserRepository;
     @PostMapping
-    public void create(@RequestBody UserModel user){
-        System.out.println(user.getUsername());
+    public UserModel create(@RequestBody UserModel user){
+        var userCreated = iUserRepository.save(user);
+        return userCreated;
     }
 }
