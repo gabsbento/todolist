4e0d61b5ba1695134b9b4006fe2ec9853e5656ab
Gabriel Bento <gab.silva.bento@gmail.com>
2024-12-05 11:27:13 -0300

    :sparkles: feat: validando as horas

--- src/main/java/br/com/gabrielsilva/todolist/task/TaskController.java
+++ src/main/java/br/com/gabrielsilva/todolist/task/TaskController.java
@@ -2,11 +2,14 @@ package br.com.gabrielsilva.todolist.task;
 
 import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.time.LocalDateTime;
 import java.util.UUID;
 
 @RestController
@@ -15,10 +18,14 @@ public class TaskController {
     @Autowired
     private ITaskRepository iTaskRepository;
     @PostMapping
-    public TaskModel create(@RequestBody TaskModel taskModel, HttpServletRequest request){
+    public ResponseEntity create(@RequestBody TaskModel taskModel, HttpServletRequest request){
         var idUser = request.getAttribute("idUser");
         taskModel.setIdUser((UUID) idUser);
+        var currentDate = LocalDateTime.now();
+        if(currentDate.isAfter(taskModel.getStartAt()) || taskModel.getStartAt().isAfter(taskModel.getEndAt())){
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("As datas estão erradas");
+        }
         var task = this.iTaskRepository.save(taskModel);
-        return task;
+        return ResponseEntity.status(HttpStatus.CREATED).body(task);
     }
 }
